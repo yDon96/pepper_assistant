798a1b28feaa74b86851245971417756e1582546
Youssef Donadeo <you.donadeo@icloud.com>
2022-01-26 17:15:07 +0100

    Clean code

--- src/nlu_server.py
+++ src/nlu_server.py
@@ -1,41 +1,40 @@
 #!/usr/bin/env python3
 from ros_pepper_pkg.srv import Dialogue, DialogueResponse
-import subprocess
 import rospy
 import requests
 import yaml
 import os
 
+def get_post_message(text):
+    return { "sender": 'bot', "message": text }
 
-def handle_service(req):
-    input_text = req.input_text   
+def get_dialogue_response_from(rest_response):
+    result = DialogueResponse()
+    result.answer = ""
+    for i in rest_response.json():
+        result.answer += i['text'] + ' ' if 'text' in i else ''
 
-    # Get answer        
-    get_answer_url = 'http://localhost:5002/webhooks/rest/webhook'
-    message = {
-        "sender": 'bot',
-        "message": input_text
-    }
+    return result
 
-    r = requests.post(get_answer_url, json=message)
-    response = DialogueResponse()
-    response.answer = ""
-    for i in r.json():
-        response.answer += i['text'] + ' ' if 'text' in i else ''
+def handle_service(req, server_url):
 
-    return response
+    message = get_post_message(req.input_text )
+    rest_response = requests.post(server_url, json=message)
+    dialogue_response = get_dialogue_response_from(rest_response)
+
+    return dialogue_response
 
 def main():
-
-    # Server Initialization
-    rospy.init_node('dialogue_service')
-
-    s = rospy.Service('dialogue_server',
-                        Dialogue, handle_service)
-
     rospy.logdebug('Dialogue server READY.')
     rospy.spin()
 
+def init_node(config):
+    service_name = config['nodes']['nluServer']
+    server_url = config['nlu']['serverUrl']
+    rospy.init_node(service_name)
+    service = rospy.Service(service_name, 
+                            Dialogue, 
+                            lambda dialogue_request : handle_service(dialogue_request, server_url))
 
 if __name__ == '__main__':
 
@@ -43,7 +42,8 @@ if __name__ == '__main__':
     with open(os.path.join(REF_PATH,'config.yml')) as file:
         config = yaml.full_load(file)
 
-    
+    init_node(config)
+
     try: 
         main()
     except rospy.ROSInterruptException as e:
