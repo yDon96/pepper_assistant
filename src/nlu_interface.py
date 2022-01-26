baf3bbcb7333221b5bda35128f48d7cdd62414c5
Youssef Donadeo <you.donadeo@icloud.com>
2022-01-26 20:55:28 +0100

    Create interface voice/terminal

--- src/nlu_interface.py
+++ src/nlu_interface.py
@@ -19,6 +19,26 @@ class TerminalInterface:
     def print_output(self,text):
         print("[OUT]:",text)
 
+class VoiceTerminalInterface:
+    '''Class implementing a voice input/terminal output interface. 
+
+    Methods
+    - get_input(self): return a string read from the voice data topic
+    - print_output(self, text): prints the text on the terminal
+
+    '''
+    def __init__(self, rospy, topic):
+        self.rospy = rospy
+        self.topic = topic
+
+    def get_input(self):
+        result = rospy.wait_for_message(self.topic, String)
+        print(f"[IN]:{result}") 
+        return result
+
+    def print_output(self,text):
+        print("[OUT]:",text)
+
 def main(service, interface):
     while not rospy.is_shutdown():
         message = interface.get_input()
