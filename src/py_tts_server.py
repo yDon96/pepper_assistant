d873f1863834e40ceedfd6cc279da03a13579641
Youssef Donadeo <you.donadeo@icloud.com>
2022-01-29 15:34:25 +0100

    Fix python to use

--- src/py_tts_server.py
+++ src/py_tts_server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import pyttsx3
 import yaml
 import os
--- src/tts_server.py
+++ src/tts_server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from naoqi import ALProxy
 from optparse import OptionParser
 from pepper_nodes.srv import *
